b2fbcf527aae70faefc0ec0f573b1740660a7b32
lishiqi.conard <lishiqi.conard@bytedance.com>
2025-06-14 00:46:04 +0800

    fix: title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "@rsbuild/core";
-    import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginReact } from "@rsbuild/plugin-react";
 
-    export default defineConfig({
-      plugins: [pluginReact()],
-      source: {
-        entry: {
-          index: './src/entry.tsx',
-        },
-      },
-    });
-      
+export default defineConfig({
+  plugins: [pluginReact()],
+  source: {
+    entry: {
+      index: "./src/entry.tsx",
+    },
+  },
+  html: {
+    title: "Easy AI - code秘密花园",
+  },
+});
