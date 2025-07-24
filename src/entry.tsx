37658d3eeb394dd258d46c7ea3b00e2777a8c652
lishiqi.conard <lishiqi.conard@bytedance.com>
2025-07-24 19:36:18 +0800

    feat: add hm.baidu.com

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-    import ReactDOM from 'react-dom/client';
-    import App from './App';
-    import './entry.css';
-    
-    
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./entry.css";
 
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(<App />);
-        
+// 添加百度统计代码
+(function () {
+  const hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?101babebc798720a3301bf9ef85b1d97";
+  const s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
